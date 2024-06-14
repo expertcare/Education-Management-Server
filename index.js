fdc480e1249051e51ec499b397d394744abd5328
expertcare <expertcarewt@gmail.com>
2024-06-14 22:38:12 +0530

    added live link and local link

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ const URL = process.env.MONGODB_URI;
 app.use(
   cors({
     origin: [
-      // "http://localhost:5173",
-      // "https://education-management-app-react-atharv.vercel.app",
+      "http://localhost:5173",
+      "https://education-management-app-react-atharv.vercel.app",
     ],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
